909c813fe69039482986e81cb3f0cc48ea6abfef
Avijeet Sachdev <info@avijeets.com>
2013-03-10 16:24:53 -0300

    changed grade variable pt.2

--- src/grade.java
+++ src/grade.java
@@ -7,57 +7,57 @@ public class grade {
         
         
         if (x >= 97.5) {
-            System.out.println("A " + x + " is an A+.");
+            System.out.println("A " + grade + " is an A+.");
         }
         
         else if (x >= 94.5) {
-            System.out.println("A " + x + " is an A.");
+            System.out.println("A " + grade + " is an A.");
         }
         
         else if (x >= 91.5) {
-            System.out.println("A " + x + " is an A-.");
+            System.out.println("A " + grade + " is an A-.");
         }
         
         
         else if (x >= 88.5) {
-            System.out.println("A " + x + " is a B+.");    
+            System.out.println("A " + grade + " is a B+.");    
         }
         
         else if (x >=85.5) {
-            System.out.println("A " + x + " is a B.");
+            System.out.println("A " + grade + " is a B.");
         }
         
         else if (x >= 82.5) {
-            System.out.println("A " + x + " is a B-.");
+            System.out.println("A " + grade + " is a B-.");
         }
         
         else if (x >= 75.5) {
-            System.out.println("A " + x + " is a C+.");
+            System.out.println("A " + grade + " is a C+.");
         }
         
         else if (x >= 71.5) {
-            System.out.println("A " + x + " is a C.");
+            System.out.println("A " + grade + " is a C.");
         }
         
         else if (x >= 69.5) {
-            System.out.println("A " + x + " is a C-.");
+            System.out.println("A " + grade + " is a C-.");
         }
         
         else if (x >= 65.5) {
-            System.out.println("A " + x + " is a D+.");
+            System.out.println("A " + grade + " is a D+.");
         }
         
         else if (x >= 61.5) {
-            System.out.println("A " + x + " is a D.");
+            System.out.println("A " + grade + " is a D.");
         }
         
         else if (x >= 59.5) {
-            System.out.println("A " + x + " is a D.");
+            System.out.println("A " + grade + " is a D.");
     
         }
         
         else {
-            System.out.println("A " + x + " is an F.");
+            System.out.println("A " + grade + " is an F.");
         }
     }
 }
